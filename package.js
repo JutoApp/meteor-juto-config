b7996aed54603344ffbec7a719237b31cf2e679f
Mike Cunneen <mike@appworkshop.net>
2017-01-25 17:40:12 +0800

    I seem to be required to bump versions every time atmosphere 'meteor publish' upload fails :\

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'juto:config',
-  version: '1.0.2',
+  version: '1.0.3',
   // Brief, one-line summary of the package.
   summary: 'Use node config package for meteor settings.',
   // URL to the Git repository containing the source code for this package.
